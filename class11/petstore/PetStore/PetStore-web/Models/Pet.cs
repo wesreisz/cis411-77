a098de85c8b035d78b78e64cecc6334ebc1d6bdd
test-again <test-again@wesleyreisz.com>
2014-03-18 17:20:37 -0700

    Setup the Database: - Reviewed the wireframes - Created a datamodel of two objects - Annotated datamodel to add a foriegn key relationship - Used NuGet and added Entity Framework - Used NuGet console and added: enable-migrations - Included reference on page - Created a Database - Created a Data Connections for the database - Add-migration pushing_model - update-database -Verbose - Confirmed data was created

---
+++ class11/petstore/PetStore/PetStore-web/Models/Pet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PetStore_web.Models
+{
+    [Table("Pet")]
+    public class Pet
+    {
+        [Key]
+        public int petId { get; set; }
+        public String name { get; set; }
+        public String description { get; set; }
+        public int quanity { get; set; }
+        public double price { get; set; }
+        public DateTime lastUpdateDate { get; set; }
+        public String smlPicture { get; set; }
+        public String lrgPicture { get; set; }
+        public int personId { get; set; }
+        [ForeignKey("personId")]
+        public virtual Person person { get; set; }
+    }
+
+    [Table("Person")]
+    public class Person
+    {
+        [Key]
+        public int personId { get; set; }
+        public String displayName { get; set; }
+        public DateTime lastUpdateDate { get; set; }
+        public String uid { get; set; }
+        public String password { get; set; }
+
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
