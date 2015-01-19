65ee233062cc728c48c16a1f9da22f5d86c4b2c2
Wesley Reisz <wes@wesleyreisz.com>
2015-01-19 15:16:07 -0500

    Homework 3 Step6: Add Logic to Prevent Reselecting previously selected cell

--- class3/scripts/tictactoe.js
+++ class3/scripts/tictactoe.js
@@ -7,6 +7,13 @@ $(document).ready(
 		
 		//use this event handler to change the background of the cell to the X or )
 		$( ".cell" ).on( "click", function() {
+		   //check to see if already selected	
+		   if($(this).hasClass("o-clicked") || $(this).hasClass("x-clicked")){
+				alert("Please select a different cell, this one has already been selected.");
+				return;
+		   }	
+		  
+		   //mark cells
 		   if("x"===tictactoe.currentMove){
 			  $(this).addClass("o-clicked");
 			  tictactoe.currentMove="o";
