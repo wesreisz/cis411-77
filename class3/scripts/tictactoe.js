3837682010c6174fbe5bc49a3fc1329b5596dfeb
Wesley Reisz <wes@wesleyreisz.com>
2015-01-19 14:52:03 -0500

    Homework 3 Step4: Make jQuery Start changing elements when clicked

--- class3/scripts/tictactoe.js
+++ class3/scripts/tictactoe.js
@@ -1,5 +1,10 @@
 $(document).ready(
 	function(){
-		console.log("loading tictactoe")
+		console.log("loading tictactoe");
+		
+		//use this event handler to change the background of the cell to the X or )
+		$( ".cell" ).on( "click", function() {
+		   $(this).addClass("x-clicked");   
+		});
 	}
 );
