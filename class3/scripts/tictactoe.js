b9dd79f01946da20de4026828b285593d7b6592e
Wesley Reisz <wes@wesleyreisz.com>
2015-01-19 17:01:41 -0500

    Homework 3 Step8: Added Game win logic and styles

--- class3/scripts/tictactoe.js
+++ class3/scripts/tictactoe.js
@@ -1,5 +1,45 @@
 var tictactoe = {};
-tictactoe.currentMove="";
+tictactoe.currentMove="x";
+
+//set our score to nothing
+tictactoe.playerscore_x=0;
+tictactoe.playerscore_o=0;
+tictactoe.WINS = [7, 56, 448, 73, 146, 292, 273, 84];
+
+tictactoe.scoreplayer=function(player,moveScore){
+	console.log("Player: " + player + " scores: " + moveScore);
+	if ("x"===player){
+		tictactoe.playerscore_x+=moveScore;
+		tictactoe.checkForWin(tictactoe.playerscore_x, "Player X");
+	}else if ("o"===player){
+		tictactoe.playerscore_o+=moveScore;
+		tictactoe.checkForWin(tictactoe.playerscore_o, "Player O");
+	}else{
+		console.log("no player was passed in.");
+	}
+	console.log("Player X: " + tictactoe.playerscore_x + "/Player O: " + tictactoe.playerscore_o);
+}
+tictactoe.checkForWin=function (totalScore, playerName){
+	 /* Check for winner
+	 *     273                 84
+	 *        \               /
+	 *          1 |   2 |   4  = 7
+	 *       -----+-----+-----
+	 *          8 |  16 |  32  = 56
+	 *       -----+-----+-----
+	 *         64 | 128 | 256  = 448
+	 *       =================
+	 *         73   146   292
+	 
+     */
+	for(var i=0;i<tictactoe.WINS.length;i++){
+		if (tictactoe.WINS[i]==totalScore){
+			$('#msgInfo').text(playerName + " Wins!!!");
+			$('#msgInfo').show();
+		}
+	}
+}
+
 
 $(document).ready(
 	function(){
@@ -13,19 +53,20 @@ $(document).ready(
 				$('#msgInfo').text("Please select a different cell, this one has already been selected.");
 				$('#msgInfo').show().delay(1500).fadeOut('slow');
 				return;
-		   }	
-		  
+		   }
+		   //get score for magic square calculation	
+		   var moveScore = $(this).data("score");  
+		
 		   //mark cells
 		   if("x"===tictactoe.currentMove){
-			  $(this).addClass("o-clicked");
+			  $(this).addClass("x-clicked");
+			  tictactoe.scoreplayer("x",moveScore);
 			  tictactoe.currentMove="o";
-		   }else if("o"===tictactoe.currentMove){
-			  $(this).addClass("x-clicked"); 
+		   }else{
+			  $(this).addClass("o-clicked"); 
+			  tictactoe.scoreplayer("o",moveScore);
 			  tictactoe.currentMove="x";	
-			}else{
-		      $(this).addClass("x-clicked"); 
-		      tictactoe.currentMove="x";	
-		   }	
+			}	
 		});
 	}
 );
