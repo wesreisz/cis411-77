e4d6d7ca9b19bde0ad101c722a8d5055f2155499
Wesley Reisz <wes@wesleyreisz.com>
2015-01-19 15:10:26 -0500

    Homework 3 Step5: Create Basic Logic for TicTacToe, no win evaluation

--- class3/scripts/tictactoe.js
+++ class3/scripts/tictactoe.js
@@ -1,10 +1,22 @@
+var tictactoe = {};
+tictactoe.currentMove="";
+
 $(document).ready(
 	function(){
 		console.log("loading tictactoe");
 		
 		//use this event handler to change the background of the cell to the X or )
 		$( ".cell" ).on( "click", function() {
-		   $(this).addClass("x-clicked");   
+		   if("x"===tictactoe.currentMove){
+			  $(this).addClass("o-clicked");
+			  tictactoe.currentMove="o";
+		   }else if("o"===tictactoe.currentMove){
+			  $(this).addClass("x-clicked"); 
+			  tictactoe.currentMove="x";	
+			}else{
+		      $(this).addClass("x-clicked"); 
+		      tictactoe.currentMove="x";	
+		   }	
 		});
 	}
 );
