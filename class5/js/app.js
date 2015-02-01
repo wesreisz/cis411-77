07db5cd43a58983806d16d2f0a59b7cce6643b73
Wesley Reisz <wes@wesleyreisz.com>
2015-02-01 12:37:31 -0500

    completed challengessection 3.8

--- class5/js/app.js
+++ class5/js/app.js
@@ -24,6 +24,14 @@
     };
   });
 
+  app.controller('ReviewController', function(){
+    this.review = {};
+	this.addReview = function(product){
+		product.reviews.push(this.review);
+		this.review = {};
+	}
+  });
+
   var gems = [
     {
       name: 'Azurite',
