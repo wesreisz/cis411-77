1fc20b1ebbac461dd6dbfac7cb9d24cc636c7cc5
Wesley Reisz <wes@wesleyreisz.com>
2015-02-03 21:03:43 -0500

    todo: created Add Method

--- class5/todo/js/app.js
+++ class5/todo/js/app.js
@@ -2,4 +2,9 @@ var app = angular.module('todo',[]);
 app.controller('TodoController', function(){
 	this.title = "Angular Todo List";
 	this.todoText = "";
+	this.todos = [];
+	this.addTodo = function(todoItem){
+		this.todos.push(todoItem);
+		this.todoText = "";
+	};
 });
