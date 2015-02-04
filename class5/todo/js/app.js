ae30edce7cfe9db1b48bf8b87e28296c89c271be
Wesley Reisz <wes@wesleyreisz.com>
2015-02-03 22:56:29 -0500

    todo: added delete function and reflowed layout

--- class5/todo/js/app.js
+++ class5/todo/js/app.js
@@ -1,20 +1,35 @@
 var app = angular.module('todo',[]);
-app.controller('TodoController', function(){
+app.controller('TodoController', ['$scope',function($scope){
 	this.title = "Angular Todo List";
 	this.todoText = "";
 	this.todos = [];
+	this.showDelete = false;
 	//adding method
 	this.addTodo = function(todoItem){
-		this.todos.push(todoItem);
-		this.todoText = "";
+		if(todoItem.length>0){
+			this.todos.push(todoItem);
+			this.todoText = "";
+		}
 	};
 	//editing method
 	this.toogleEdit = function(todoItem){
 		this.todoText = todoItem;
+		this.deleteItem(todoItem);
+	}
+	//toogle delete action
+    this.toogleDelete = function(todoItem){
+		if(!this.showDelete){
+			this.showDelete = true;
+		}else{
+			this.showDelete = false;
+		}
+	}
+	//deleting method
+	this.deleteItem = function(todoItem){
 		var pos = this.todos.indexOf(todoItem);
 		if (pos > -1) {
 		    this.todos.splice(pos, 1);
 		}
-
 	}
-});
+}]);
+
