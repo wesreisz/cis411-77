0c926801b797ec323848ef187b1f284dc451839f
Wesley Reisz <wes@wesleyreisz.com>
2015-02-03 21:32:08 -0500

    todo: created edit method, leveraging jquery

--- class5/todo/js/app.js
+++ class5/todo/js/app.js
@@ -3,8 +3,18 @@ app.controller('TodoController', function(){
 	this.title = "Angular Todo List";
 	this.todoText = "";
 	this.todos = [];
+	//adding method
 	this.addTodo = function(todoItem){
 		this.todos.push(todoItem);
 		this.todoText = "";
 	};
+	//editing method
+	this.toogleEdit = function(todoItem){
+		this.todoText = todoItem;
+		var pos = this.todos.indexOf(todoItem);
+		if (pos > -1) {
+		    this.todos.splice(pos, 1);
+		}
+
+	}
 });
