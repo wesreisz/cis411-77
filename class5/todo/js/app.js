2c171b49e28ea76aecbd7a0159ba9fd7002de83e
Wesley Reisz <wes@wesleyreisz.com>
2015-02-04 19:54:26 -0500

    todo: toggle controls on and off

--- class5/todo/js/app.js
+++ class5/todo/js/app.js
@@ -3,8 +3,16 @@ app.controller('TodoController',function(){
 	this.title='Todo List';
 	this.textInput='';
 	this.todos = ['wes','kim'];
+	this.isControlsShown = false;
 	this.addItem = function(item){
 		this.todos.push(item);
 		this.textInput='';
+	};
+	this.toggleControls = function(){
+		if(!this.isControlsShown){
+			this.isControlsShown = true;
+		}else{
+			this.isControlsShown = false;
+		} 
 	};
 });
