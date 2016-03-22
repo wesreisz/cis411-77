b83d32c71ad90170267d1ec6632c9f14b8c47e29
Wesley Reisz <wes@wesleyreisz.com>
2016-03-22 17:49:45 -0500

    Added Code First Entity without Repository

---
+++ class12/SampleEntityExampleMinusRepository/MinusRepositorySample/MinusRepositorySample/Grade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MinusRepositorySample
+{
+    public class Grade
+    {
+        public Grade() { }
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
