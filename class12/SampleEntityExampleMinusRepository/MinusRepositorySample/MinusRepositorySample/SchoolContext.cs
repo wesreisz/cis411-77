18e5278d87138ad562a38f4434046c1c587f5519
Wesley Reisz <wes@wesleyreisz.com>
2016-03-22 18:11:26 -0500

    Updated Project

---
+++ class12/SampleEntityExampleMinusRepository/MinusRepositorySample/MinusRepositorySample/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinusRepositorySample
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base() { }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
