84cbf7b88e91414ced3acbbd685bb668edb84a3a
Wesley Reisz <wes@wesleyreisz.com>
2016-03-22 18:15:52 -0500

    DB First Example

---
+++ class12/DbFirstExample/DBFirst/DBFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Entities()) {
+                foreach (Student student in context.Students) {
+                    Console.WriteLine("Student {0} : {1}", student.StudentID, student.StudentName);
+                }
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.Read();
+        }
+    }
+}
